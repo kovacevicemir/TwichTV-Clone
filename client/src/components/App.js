6fe35cf5692f6065ec609451a352a829ca7ec54b
kovacevicemir <emirkovacevic92@gmail.com>
2020-05-31 03:31:34 +1000

    React Router added and components connection

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamList from './streams/StreamList'
+import StreamDelete from './streams/StreamDelete'
+import Header from './Header'
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
