64c2f6b32b5a0b1b0293831b0407611125fd611e
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-01 23:53:38 +1000

    streamReducer and actions added, list of streams render

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import {fetchStreams} from '../../actions'
+import {connect} from 'react-redux'
 
-const StreamList = ()  =>{
-        return (
+class StreamList extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchStreams()
+    }
+
+    renderList(){
+        return this.props.streams.map(stream =>{
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render(){
+        return(
             <div>
-                StreamList
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
             </div>
         )
+    }
 }
 
-export default StreamList
+
+//Object.values will take obj state.streams and turn it into array...
+const mapStateToProps = (state) =>{
+    return {streams: Object.values(state.streams)}
+}
+
+
+export default connect(
+    mapStateToProps,
+    {fetchStreams: fetchStreams}
+)(StreamList)
