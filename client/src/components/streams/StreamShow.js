03039d4ba051f29f6940f59db1935c4df09f9054
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-13 17:09:39 +1000

    adding RTMP server, OBS setup, FLV js... some bug here...

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import flv from 'flv.js'
 import {fetchStream} from '../../actions'
 
 class StreamShow extends Component {
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef()
+    }
 
     componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id)
+        const {id} = this.props.match.params.id
+
+        this.props.fetchStream(id)
+        this.buildPlayeR()
     }
+    buildPlayeR(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        const {id} = this.props.match.params
+
+        this.player = flv.createPlayer({
+            type:'flv',
+            url:`http://localhost:8000/live/${id}.flv`
+        })
+
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
+    }
+
+    componentDidUpdate(){
+        this.buildPlayeR()
+    }
+
 
     render() {
         if(!this.props.stream){
@@ -17,12 +45,9 @@ class StreamShow extends Component {
 
         return (
             <div>
-                <h1>
-                    {title}
-                </h1>
-                <h5>
-                    {description}
-                </h5>
+                <video  ref={this.videoRef} style={{width: '100%'}} controls={true}/>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
             </div>
         )
     }
